e918323261158163e2326492666ab5eea611e17c
Andrei Orlov <aaorlov1@gmail.com>
2025-04-15 17:54:21 +0300

    Update examples

--- examples/example-smart-contract/src/main.rs
+++ examples/example-smart-contract/src/main.rs
@@ -8,9 +8,10 @@ extern "C" fn main(op: u32) -> u64 {
     match op {
         0 => call_transfer(),
         1 => call_balance(),
-        2 => call_balance_of(),
         3 => call_block_number(),
+        2 => call_balance_of(),
         4 => call_infinity_loop(),
+        5 => call_inc(),
         _ => unimplemented!(),
     }
 }
@@ -33,4 +34,23 @@ fn call_infinity_loop() -> u64 {
 
 fn call_transfer() -> u64 {
     unsafe { transfer(2, 0) }
+}
+
+fn call_inc() -> u64 {
+    let mut buffer = [0u8; 8];
+    let pointer: *mut [u8; 8] = &mut buffer;
+
+    unsafe {
+        if get(pointer as u32, 0u32) != 0 {
+            return 1
+        };
+
+        buffer[7] += 1;
+
+        if set(pointer as u32, 0u32) != 0 {
+            return 1
+        }
+    }
+
+    0
 }
