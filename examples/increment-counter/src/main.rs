59021d24dc9e1b12b1273fb6248d24140e6c5081
Sviatoslav Alekseev <sviatoslav.alekseev@proton.me>
2025-05-22 13:56:00 +0500

    Add print example

--- examples/increment-counter/src/main.rs
+++ examples/increment-counter/src/main.rs
@@ -5,6 +5,11 @@ use parity_scale_codec::{Decode, Encode};
 
 qf_polkavm_sdk::host_functions!();
 
+const STORAGE_KEY_LEN: usize = 256;
+type StorageKey = [u8; STORAGE_KEY_LEN];
+const READ_BUFFER_LEN: usize = 2048;
+type ReadBuffer = [u8; READ_BUFFER_LEN];
+
 #[derive(Encode, Decode, Default)]
 struct Counter {
     counter: u32,
@@ -12,7 +17,7 @@ struct Counter {
 
 #[polkavm_derive::polkavm_export]
 extern "C" fn main() -> u64 {
-    let mut buffer = [0u8; 64];
+    let mut buffer = [0u8; READ_BUFFER_LEN];
     if let Err(err) = get_input_data(&mut buffer) {
         return err;
     }
@@ -22,8 +27,8 @@ extern "C" fn main() -> u64 {
     call_increment(increment)
 }
 
-fn get_input_data(buffer: &mut [u8; 64]) -> Result<(), u64> {
-    let pointer: *mut [u8; 64] = buffer;
+fn get_input_data(buffer: &mut ReadBuffer) -> Result<(), u64> {
+    let pointer: *mut ReadBuffer = buffer;
 
     match unsafe { get_user_data(pointer as u32) } {
         0 => Ok(()),
@@ -32,17 +37,31 @@ fn get_input_data(buffer: &mut [u8; 64]) -> Result<(), u64> {
 }
 
 fn call_increment(increment: u32) -> u64 {
-    // "                                                             foo"
-    // "0x20202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020666F6F"
-    let storage_key: [u8; 64] = [
-        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 102, 111, 111,
-    ];
-    let storage_key_pointer: *const [u8; 64] = &storage_key;
+    // string representation of the key used to store the counter value
+    // "                                                                                                                                                                                                                                                             foo"
 
-    let mut buffer = [0u8; 64];
-    let pointer: *mut [u8; 64] = &mut buffer;
+    // hex representation of the key used to store the counter value
+    // "0x20202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020666F6F"
+
+    // binary representation of the key used to store the counter value
+    let storage_key: StorageKey = [
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        102, 111, 111,
+    ];
+    let storage_key_pointer: *const StorageKey = &storage_key;
+
+    let mut buffer = [0u8; READ_BUFFER_LEN];
+    let pointer: *mut ReadBuffer = &mut buffer;
 
     unsafe {
         match get(storage_key_pointer as u32, pointer as u32) {
@@ -51,7 +70,7 @@ fn call_increment(increment: u32) -> u64 {
         }
 
         let mut tmp: &[u8] = &buffer;
-        let mut counter = if buffer == [0u8; 64] {
+        let mut counter = if buffer == [0u8; READ_BUFFER_LEN] {
             Counter::default()
         } else {
             Counter::decode(&mut tmp).unwrap_or(Counter::default())
--- src/lib.rs
+++ src/lib.rs
@@ -136,7 +136,7 @@ macro_rules! host_functions {
         extern "C" {
             fn balance() -> u64;
             fn balance_of() -> u64;
-            fn print() -> u64;
+            fn print(msg_pointer: u32, len: u32) -> u64;
             fn transfer(address_idx: u32, balance_idx: u32) -> u64;
             fn block_number() -> u64;
             fn account_id() -> u64;
