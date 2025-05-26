384507aa22c83c65b6a049a0b8e336bb2ea5f2e6
Sviatoslav Alekseev <sviatoslav.alekseev@proton.me>
2025-05-26 11:29:17 +0200

    Fix typo

--- examples/get-contract-account/src/main.rs
+++ examples/get-contract-account/src/main.rs
@@ -9,12 +9,12 @@ qf_polkavm_sdk::host_functions!();
 
 #[polkavm_derive::polkavm_export]
 extern "C" fn main() -> u64 {
-    let contract = get_conatract_account();
+    let contract = get_contract_account();
     let msg = format!("contract: {contract}");
     call_print(&msg)
 }
 
-fn get_conatract_account() -> u64 {
+fn get_contract_account() -> u64 {
     unsafe { account_id() }
 }
 
