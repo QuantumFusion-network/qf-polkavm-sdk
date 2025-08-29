2f1caa4a64428654c3842a559e40f5c15546827c
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-08-29 12:00:38 +0500

    Preserve fn attrs

--- qf-polkavm-sdk/procedural/src/lib.rs
+++ qf-polkavm-sdk/procedural/src/lib.rs
@@ -10,11 +10,13 @@ use syn::{parse_macro_input, ItemFn};
 pub fn export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(item as ItemFn);
     
+    let fn_attrs = &input_fn.attrs;
     let fn_vis = &input_fn.vis;
     let fn_sig = &input_fn.sig;
     let fn_block = &input_fn.block;
     
     let expanded = quote! {
+        #(#fn_attrs)*
         #[no_mangle]
         #[polkavm_derive::polkavm_export]
         #fn_vis extern "C" #fn_sig #fn_block
