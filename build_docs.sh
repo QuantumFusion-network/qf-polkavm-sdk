155b235df000c066424a915998583d791a742c21
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-11-27 15:41:10 +0500

    Fix build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -4,11 +4,17 @@ set -euo pipefail
 
 echo "Building documentation for all workspace members..."
 
+TARGET_JSON_PATH=$(polkatool get-target-json-path)
+
+# Fix for rustc `target-pointer-width` type breaking change, see
+# https://github.com/paritytech/polkadot-sdk/issues/10400
+sed -i 's/"target-pointer-width": "64",/"target-pointer-width": 64,/' $TARGET_JSON_PATH
+
 RUSTFLAGS="--remap-path-prefix=$(pwd)= --remap-path-prefix=${HOME}=~" \
     cargo +nightly doc \
         --workspace \
         --no-deps \
         -Z build-std=core,alloc \
-        --target $(polkatool get-target-json-path)
+        --target $TARGET_JSON_PATH
 
 echo "Documentation build complete!"
