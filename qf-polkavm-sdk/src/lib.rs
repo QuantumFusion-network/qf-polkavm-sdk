14b4e4eeb9a412d2dedf5fad39009444bbdb58df
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-12-02 11:56:09 +0500

    Add crate level doc comment

--- qf-polkavm-sdk/src/lib.rs
+++ qf-polkavm-sdk/src/lib.rs
@@ -1,3 +1,7 @@
+//! Native Rust smart contracts SDK for QF Network. Smart contract functionality in QF Network is
+//! implemented using `pallet-revive` from Polkadot SDK / Substrate and this SDK should work for
+//! creating smart contracts for other blockchains that integrate `pallet-revive` as well.
+
 #![no_std]
 #![no_main]
 
